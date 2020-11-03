71bfb5b20b7ac1c90fc77c770465cc59927f3e3a
Aishwarya Ramanathan <cutieice@gmail.com>
2020-11-03 13:44:29 -0500

    Decoupling of learning and applying is complete

---
+++ SpacerTransformationsAPI/SpacerTransformationsAPI/Models/ApplyTransformRequestBody.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SpacerTransformationsAPI.Models
+{
+    public class ApplyTransformRequestBody
+    {
+        [JsonProperty(PropertyName = "instance")]
+        public string Instance { get; set; }
+        [JsonProperty(PropertyName = "program")]
+        public string Program { get; set; }
+        [JsonProperty(PropertyName = "declareStatements")]
+        public List<string> DeclareStatements { get; set; }
+    }
+}
