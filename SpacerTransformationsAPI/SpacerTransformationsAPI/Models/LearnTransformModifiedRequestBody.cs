5bb4e487dd73b71fdb8b84bc0be7b2c3c6f7f6e1
Aishwarya Ramanathan <cutieice@gmail.com>
2020-11-30 20:45:11 -0500

    Added modified endpoint for learning transformations

---
+++ SpacerTransformationsAPI/SpacerTransformationsAPI/Models/LearnTransformModifiedRequestBody.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SpacerTransformationsAPI.Models
+{
+    public class LearnTransformModifiedRequestBody
+    {
+        
+        [JsonProperty(PropertyName = "instance")]
+        public string Instance { get; set; }
+        [JsonProperty(PropertyName = "declareStatements")]
+        public string DeclareStatements { get; set; }
+        [JsonProperty(PropertyName = "inputOutputExamples")]
+        public List<TrainingInputOutput> InputOutputExamples { get; set; }
+    }
+}
