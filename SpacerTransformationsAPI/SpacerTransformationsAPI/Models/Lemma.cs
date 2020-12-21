f65c4616d4154e9c24179f2b0d83b7815bebd467
Aishwarya Ramanathan <cutieice@gmail.com>
2020-12-21 16:53:22 -0500

    Fixed backend according to info coming from front end editor modal

--- SpacerTransformationsAPI/SpacerTransformationsAPI/Models/Lemma.cs
+++ SpacerTransformationsAPI/SpacerTransformationsAPI/Models/Lemma.cs
@@ -4,11 +4,8 @@ namespace SpacerTransformationsAPI.Models
 {
     public class Lemma
     {
-        public string Edited { get; set; }
         public string Raw { get; set; }
         public string Readable { get; set; }
-        public List<int> Lhs { get; set; }
-        public bool Changed { get; set; }
         
     }
 }
