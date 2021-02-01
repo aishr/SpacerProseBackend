f184c69bb4cb9144fb6bc815c10b9ea396dea8dd
Nham Le <nv3le@uwaterloo.ca>
2021-02-01 17:32:08 -0500

    cleanup DynamoDb

--- SpacerTransformationsAPI/SpacerTransformationsAPI/Functions/DynamoDb.cs
+++ SpacerTransformationsAPI/SpacerTransformationsAPI/Functions/DynamoDb.cs
@@ -1,92 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Amazon;
-using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.Model;
-using Amazon.Runtime;
 using Microsoft.Z3;
 using SpacerTransformationsAPI.Models;
+using Newtonsoft.Json;
+
 
 namespace SpacerTransformationsAPI.Functions
 {
     public static class DynamoDb
     {
-        private static readonly AmazonDynamoDBClient Client = new AmazonDynamoDBClient(
-            new BasicAWSCredentials(Environment.GetEnvironmentVariable("ACCESS_KEY"),
-                Environment.GetEnvironmentVariable("SECRET_KEY")), 
-            RegionEndpoint.USEast2);
-        
-        public static async Task<GetItemResponse> GetLemmas(string id)
-        {
-            var key = new Dictionary<string, AttributeValue>()
-            {
-                {"Id", new AttributeValue(id)}
-            };
-            var request = new GetItemRequest(Environment.GetEnvironmentVariable("TABLE_NAME"), key);
-
-            return await Client.GetItemAsync(request);
-        }
-
-        public static SpacerInstance GetChangedLemmas(GetItemResponse response)
-        {
-            var results = new SpacerInstance(response.Item["Id"].S);
-            foreach (var lemmaNum in response.Item)
-            {
-                if (lemmaNum.Key == "Id") continue;
-                if (lemmaNum.Value.M["changed"].BOOL)
-                {
-                    results.Lemmas.Add(int.Parse(lemmaNum.Key), CreateLemma(lemmaNum.Value));
-                }
-            }
-
-            return results;
-        }
-
-        public static SpacerInstance DbToSpacerInstance(GetItemResponse response)
-        {
-            var results = new SpacerInstance(response.Item["Id"].S);
-            foreach (var lemmaNum in response.Item)
-            {
-                if (lemmaNum.Key == "Id") continue;
-                results.Lemmas.Add(int.Parse(lemmaNum.Key), CreateLemma(lemmaNum.Value));
-            }
-            return results;
-        }
-
-        private static Lemma CreateLemma(AttributeValue itemValue)
-        {
-            return new Lemma() 
-            {
-                 Readable = itemValue.M["readable"].S,
-                 Raw = itemValue.M["raw"].S,
-            };
-            
-        }
-
-        /*
-        public static IEnumerable<Tuple<Node, Node>> GetInputOutputExamples(Context ctx, IEnumerable<Lemma> lemmas, string prefix, string declareStatements)
-        {
-            var results = new List<Tuple<Node, Node>>();
-             foreach (var lemma in lemmas)
-             {
-                 if (!lemma.Changed) continue;
-                 var input = SmtLib.StringToSmtLib(ctx, string.Format(prefix, declareStatements, lemma.Raw));
-                 var inputTree = Utils.HandleSmtLibParsed(input, ctx);
-                 var outputTree = Semantics.Transform(inputTree, lemma.Lhs);
-                 results.Add(new Tuple<Node, Node>(inputTree, outputTree));
-             }
-
-             return results;
-        }
-        */
-
         public static IEnumerable<Tuple<Node, Node>> GetInputOutputExamplesModified(Context ctx,
             List<TrainingInputOutput> trainingExamples, string prefix, string declareStatements)
         {
             var results = new List<Tuple<Node, Node>>();
              foreach (var example in trainingExamples)
              {
+                 Console.WriteLine(example);
                  var input = SmtLib.StringToSmtLib(ctx, string.Format(prefix, declareStatements, example.Input));
                  var inputTree = Utils.HandleSmtLibParsed(input, ctx);
                  var output = SmtLib.StringToSmtLib(ctx, string.Format(prefix, declareStatements, example.Output));
@@ -95,7 +25,6 @@ namespace SpacerTransformationsAPI.Functions
              }
 
              return results;
-            
         }
     }
 }
