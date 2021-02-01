ab7734337629203af9dc309e717d3fd5a13b6825
Nham Le <nv3le@uwaterloo.ca>
2021-02-01 16:09:59 -0500

    clean up TController

--- SpacerTransformationsAPI/SpacerTransformationsAPI/Controllers/TransformationsController.cs
+++ SpacerTransformationsAPI/SpacerTransformationsAPI/Controllers/TransformationsController.cs
@@ -53,9 +53,9 @@ namespace SpacerTransformationsAPI.Controllers
         [HttpPost]
         public ActionResult LearnTransformationModified([FromBody]LearnTransformModifiedRequestBody requestBody)
         {
+
             try
             {
-                Console.WriteLine(requestBody.Instance);
 
                 ProgramSet learned;
                 using (var ctx = new Context())
@@ -93,7 +93,7 @@ namespace SpacerTransformationsAPI.Controllers
         }
         
         [HttpPost]
-        public async Task<ActionResult> ApplyTransformation([FromBody]ApplyTransformRequestBody requestBody)
+        public ActionResult ApplyTransformation([FromBody]ApplyTransformRequestBody requestBody)
         {
             try
             {
@@ -101,24 +101,57 @@ namespace SpacerTransformationsAPI.Controllers
                 Console.WriteLine(requestBody.Program);
                 var finalProgram = ProgramNode.Parse(requestBody.Program, _grammar.Value);
 
-                var rawLemmas = await DynamoDb.GetLemmas(requestBody.Instance);
-                var lemmas = DynamoDb.DbToSpacerInstance(rawLemmas);
+                var rawLemmas = requestBody.ExprMap;
+                var instance = requestBody.Instance;
+                // var lemmas = DynamoDb.DbToSpacerInstance(instance, rawLemmas);
+                var lemmas = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(rawLemmas);
+                var exprMap = new SpacerInstance(instance);
                 using (var ctx = new Context())
                 {
-                    foreach (var kvp in lemmas.Lemmas)
+                    foreach (var kvp in lemmas)
                     {
-                        if (kvp.Value.Raw != "")
-                        {
-                            var parsedSmtLib =
-                                SmtLib.StringToSmtLib(ctx, string.Format(SmtPrefix, requestBody.DeclareStatements, kvp.Value.Raw));
-                            if (parsedSmtLib.FuncDecl.DeclKind == Z3_decl_kind.Z3_OP_OR)
-                            {
-                                var input = Utils.HandleSmtLibParsed(parsedSmtLib, ctx);
-                                var stateInput = State.CreateForExecution(_grammar.Value.InputSymbol, input);
-                                var result = (Node) finalProgram.Invoke(stateInput);
+                        var raw = "";
+                        var readable = "";
+                        Console.WriteLine("kvp", kvp);
+                        foreach(var lemma_kvp in kvp.Value){
+                            if (lemma_kvp.Key == "raw"){
+                                raw = (string)lemma_kvp.Value;
+                            }
+                            if(lemma_kvp.Key == "readable"){
+                                readable = (string)lemma_kvp.Value;
+                            }
 
-                                lemmas.Lemmas[kvp.Key].Raw = result.Expr.ToString();
-                                lemmas.Lemmas[kvp.Key].Readable = ReadableParser.ParseResult(result.Expr);
+                        }
+                        if (raw != "")
+                        {
+                            try{
+                                //add try catch to handle stuff that cannot be parsed
+                                var parsedSmtLib =
+                                    SmtLib.StringToSmtLib(ctx, string.Format(SmtPrefix, requestBody.DeclareStatements, raw));
+                                if (parsedSmtLib.FuncDecl.DeclKind == Z3_decl_kind.Z3_OP_OR)
+                                {
+                                    var input = Utils.HandleSmtLibParsed(parsedSmtLib, ctx);
+                                    var stateInput = State.CreateForExecution(_grammar.Value.InputSymbol, input);
+                                    var result = (Node) finalProgram.Invoke(stateInput);
+
+                                    exprMap.Lemmas.Add(int.Parse(kvp.Key), new Lemma(){
+                                            Raw = result.Expr.ToString(),
+                                                Readable = ReadableParser.ParseResult(result.Expr),
+                                                }
+                                    );
+                                    Console.WriteLine(result.Expr);
+                                }
+
+                            }
+                            catch (Exception ex){
+                                //something wrong
+                                Console.WriteLine(ex);
+                                exprMap.Lemmas.Add(int.Parse(kvp.Key), new Lemma(){
+                                        Raw = raw,
+                                            Readable = readable,
+                                            }
+                                );
+
                             }
 
                         }
@@ -126,7 +159,7 @@ namespace SpacerTransformationsAPI.Controllers
                     ctx.Dispose();
                 }
                 Console.WriteLine("Transformation complete");
-                return Ok(JsonConvert.SerializeObject(lemmas.Lemmas));
+                return Ok(JsonConvert.SerializeObject(exprMap.Lemmas));
             }
             catch (Exception ex)
             {
@@ -136,47 +169,5 @@ namespace SpacerTransformationsAPI.Controllers
                 return StatusCode(500, ex.Message);
             }
         }
-
-        /*
-        [HttpPost]
-        public async Task<ActionResult> LearnTransformation([FromBody]LearnTransformRequestBody requestBody)
-        {
-            try
-            {
-                Console.WriteLine(requestBody.Instance);
-
-                var rawLemmas = await DynamoDb.GetLemmas(requestBody.Instance);
-                var lemmas = DynamoDb.GetChangedLemmas(rawLemmas);
-                ProgramSet learned;
-                using (var ctx = new Context())
-                {
-                    var inputExamples = DynamoDb.GetInputOutputExamples(ctx, lemmas.Lemmas
-                        .Select(kvp => kvp.Value).ToList(), SmtPrefix, requestBody.DeclareStatements);
-                    var spec = Utils.CreateExampleSpec(_grammar, inputExamples);
-                    RankingScore.ScoreForContext = 100;
-                    var scoreFeature = new RankingScore(_grammar.Value);
-                    DomainLearningLogic learningLogic = new WitnessFunctions(_grammar.Value);
-                    _prose = new SynthesisEngine(_grammar.Value,
-                        new SynthesisEngine.Config
-                        {
-                            LogListener = new LogListener(),
-                            Strategies = new ISynthesisStrategy[] {new DeductiveSynthesis(learningLogic)},
-                            UseThreads = false,
-                            CacheSize = int.MaxValue
-                        });
-                    learned = _prose.LearnGrammarTopK(spec, scoreFeature);
-                }
-
-                var finalPrograms = learned.RealizedPrograms.Select(program => new FinalProgram(program.ToString(), program.PrintAST())).ToList();
-                return Ok(JsonConvert.SerializeObject(finalPrograms));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-                return StatusCode(500, ex.Message);
-            }
-        }
-        */
-
     }
 }
